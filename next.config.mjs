1267a82084c515fa5a5caee375cab47791cb4c0e
Yasir <yasirzinbox@gmail.com>
2024-11-05 20:38:00 +0500

    updated config.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', // Enable static HTML export
+  images: {
+    unoptimized: true, // Disable image optimization
+  },
 };
 
 export default nextConfig;
