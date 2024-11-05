428747d1c68013e0e03c49e6523d226d877020d3
Yasir <yasirzinbox@gmail.com>
2024-11-05 20:40:54 +0500

    Revert "updated config."
    This reverts commit 1267a82084c515fa5a5caee375cab47791cb4c0e.

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export', // Enable static HTML export
-  images: {
-    unoptimized: true, // Disable image optimization
-  },
 };
 
 export default nextConfig;
